2d83f2f1d9790d0cc4008d1ffe034a6e2898e1c5
enquire07 <145021641+enquire07@users.noreply.github.com>
2024-06-09 09:48:54 +0800

    updated

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  const generateRandomColor = () => {
+    return `rgb(${Math.floor(Math.random() * 235)}, ${Math.floor(
+      Math.random() * 236
+    )},
+        ${Math.floor(Math.random() * 256)})`;
+  };
+
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+
+      <ul classname={css.statlist}>
+        {stats.map(stat => (
+          <li
+            className={css.item}
+            style={{
+              backgroundColor: generateRandomColor(),
+            }}
+            key={stat.id}
+          >
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
